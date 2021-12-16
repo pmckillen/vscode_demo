58c0701791d8272aa862ec495e3d12b5debd81fd
Pat McKillen <pat_mckillen@outlook.com>
2021-12-16 16:52:41 +0000

    removed some code

--- script.py
+++ script.py
@@ -2,21 +2,9 @@
 high level support for doing this and that.
 """
 
-import sys
 
 import requests
 
-print(sys.version)
-print(sys.executable)
 
-
-def greet(who):
-
-    greeting = f"Hello, {who}"
-    return greeting
-
-
-print(greet("World"))
-print(greet("Pat"))
 r = requests.get("https://bbc.co.uk")
 print(r.status_code)
